15179a97f39896aff4c1a0120d60d12d77a4b762
ARBIND LOCHAN MISHRA <almbbsr456@gmail.com>
2022-01-12 02:44:42 +0530

    :sparkles: auto saving data to database

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
-const app = express();
+const sensorsdata = require("../backend/models/sensordataModel");
+const moment = require('moment-timezone');
 require("dotenv").config();
-let sensor_reading = 0;
-
+const app = express();
 app.use(cors())
 
+var minutes = 0.1, the_interval = minutes * 60 * 1000;
+let sensor_reading = 0;
 const PORT = process.env.PORT || 5001;
 
-app.get('/', (req, res) => {
+mongoose.connect(
+    process.env.MONGODB_CONNECTION_STRING,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err) => {
+      if (err) throw err;
+      console.log("MongoDB connection established");
+    }
+);
+
+//validation
+
+app.get('/', (req, res) => {      
     res.send('Server Working')
 })
 
-app.post("/getdatasensor", async (req, res) => {
+//data from arduino
+
+app.post("/getdatasensor", async (req, res) => {   
     try {
         let body = [];
         req.on("data", (chunk) => {
@@ -31,10 +48,52 @@ app.post("/getdatasensor", async (req, res) => {
         res.status(500).json({state: err.message });
     }
 });
-  
-app.get('/getdatafrontend', (req, res) => {
+
+//sending data to frontnend
+
+app.get('/getdatafrontend', (req, res) => {         
     res.status(200).json({data : sensor_reading});
 })
+
+// auto save values(will always run)
+
+let temp;
+let flag1 = true,flag2 = false; 
+var autoadddata = function() {
+    const dateIndia = moment.tz(Date.now(), "Asia/Kolkata");
+    var date = dateIndia.toString();
+    let x = date.indexOf('GMT');    //parsing
+    date = date.substring(0, x).trim(); 
+    if(sensor_reading != 0){
+    // console.log(the_interval);
+    try{
+        const newSensorData = new sensorsdata({sensor_reading,date});
+        newSensorData.save();
+    }
+    catch (err) {
+        console.log(err.message)
+    }
+    if(sensor_reading == temp){
+        if(flag1){
+        the_interval = the_interval * 2;
+        flag1 = false;
+        flag2 = true;
+        }
+    }
+    else{
+    if(flag2){
+        the_interval = the_interval / 2;
+        flag1 = true;
+        flag2 = false;
+    }
+  }
+
+}
+temp = sensor_reading;
+setTimeout(autoadddata, the_interval);
+}
+
+setTimeout(autoadddata, the_interval);
 
 app.listen(PORT, ()=> {
     console.log(`app is running on port ${PORT}`);
---
+++ models/sensordataModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const moment = require('moment-timezone');
+
+
+const sensordataSchema = new mongoose.Schema({
+    sensor_reading : { type: String, required: true },
+    date: {type: String, required: true},   
+});
+  
+module.exports = sensorsdata = mongoose.model("sensorData", sensordataSchema);
+  
